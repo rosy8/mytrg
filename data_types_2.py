456f9aa9df2db4783cd9fe75a37e47fcdb0d1a8c
Mahesh Vaidya <mvaidya@advaoptical.com>
2017-02-14 17:29:45 +0530

    More data types

---
+++ data_types_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Array - is an ordered collection of elements
+
+myarr = [1, 2, "git", "Gerrit", "Python", "node.js"]
+#Note - mix data types are allowed ; however we will not put it
+
+
+#Tuple -
+# Definition - a
+
+
+
+#- Dictionaries
+
+
+
+# Exceptions (minimim)
